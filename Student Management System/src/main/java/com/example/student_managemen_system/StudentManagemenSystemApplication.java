34402eb23c9571914306c025335779f26785ec1d
Tharindu Kavishna <kavishna@incubatelabs.com>
2021-08-28 15:02:41 +0530

    student managemen system complted

--- Student Management System/src/main/java/com/example/student_managemen_system/StudentManagemenSystemApplication.java
+++ Student Management System/src/main/java/com/example/student_managemen_system/StudentManagemenSystemApplication.java
@@ -1,13 +1,15 @@
 package com.example.student_managemen_system;
 
+import com.example.student_managemen_system.entity.Student;
+import com.example.student_managemen_system.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagemenSystemApplication {
-
+public class StudentManagemenSystemApplication{
     public static void main(String[] args) {
         SpringApplication.run(StudentManagemenSystemApplication.class, args);
     }
-
 }
--- Student Management System/src/main/java/com/example/student_managemen_system/service/StudentService.java
+++ Student Management System/src/main/java/com/example/student_managemen_system/service/StudentService.java
@@ -1,4 +1,13 @@
 package com.example.student_managemen_system.service;
+import com.example.student_managemen_system.entity.Student;
+import java.util.List;
 
 public interface StudentService {
+    List<Student> getAllStudent();
+    Student saveStudent(Student student);
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
 }
